cdad6b6b6b65f3b9448996fcc3fe1b6d1bd9f73c
Micaela Gomez <mica16gomez@gmail.com>
2026-06-10 20:13:01 -0300

    Integracion ramas

--- backend/src/modules/gestion/dtos/input/create-proyecto.dto.ts
+++ backend/src/modules/gestion/dtos/input/create-proyecto.dto.ts
@@ -1,16 +1,25 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsNotEmpty, IsNumber, IsOptional, IsString } from "class-validator";
+import {
+  IsDateString,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from "class-validator";
 
 export class CreateProyectoDto {
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  nombre!: string;
 
-    @ApiProperty()
-    @IsString()
-    @IsNotEmpty()
-    nombre!: string;
+  @ApiProperty()
+  @IsNumber()
+  @IsOptional()
+  idCliente!: number;
 
-    @ApiProperty()
-    @IsNumber()
-    @IsOptional()
-    idCliente!: number;
-
+  @ApiProperty({ example: "2026-06-12" })
+  @IsDateString()
+  @IsNotEmpty()
+  fechaFinalizacion!: string;
 }
